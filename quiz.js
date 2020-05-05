b578d6144fb6e6f6fe6f3f63652172700be2e50d
Helen Darden <hgdarden@gmail.com>
2020-05-04 22:13:00 -0400

    est layout, start of JS

---
+++ quiz.js
@@ -0,0 +1,19 @@
+//questions need to show up on screen in index.html
+
+//questions and answers
+var quiz = [{question: "In what year was Mickey Mouse first introduced to the public.", options: ["1958", "1982", "1930", "1928"] (answers[3]) },
+                {question: "What was Micky Mouse's original name?", options: ["Steamboat Willie", "Mickah Mouse", "Captain Steve", "Mickey"] (answers[0])},
+                {question: "What is Micky Mouse's girlfriend, Minnie Mouse's full name?", options:["Minnie Mouse", "Minerva Mouse", "Minilla Mouse", "Eminem"] (answers[1]) },
+                {question: "What is Mickey Mouse's dog name?", options: ["Jupiter", "Comet", "Pluto", "Venus"] (answers[2])}
+            ];
+
+            console.log(questions);
+//score, not sure how that is going to track with the timer
+var score = 0;
+
+var question = 0;
+var options = 0;
+var answers = 0;
+var optionSelected = [];
+
+
