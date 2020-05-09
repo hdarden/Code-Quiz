e3a5c80d321c03e2a27a588eefda02e2e78e3e53
Helen Darden <hgdarden@gmail.com>
2020-05-09 16:09:55 -0400

    updated quiz layout

--- script.js
+++ script.js
@@ -1,79 +1,109 @@
-//variables pulled from index.html
+// //variables pulled from index.html
 
 var openingPage = document.getElementById("openingPage");
+var answers = document.getElementById("answers");
 var instructions = document.getElementById("instructions");
 var timer = document.getElementById("timer");
 var startButton = document.getElementById("button");
-
+var buttonDiv = document.getElementById("buttonDiv");
 
 
 //questions and answers
-var quiz = [
-  {
-    question: "In what year was Mickey Mouse first introduced to the public.",
-    options: ["1958", "1982", "1930", "1928"],
-    answers: 3,
-  },
-  {
-    question: "What was Micky Mouse's original name?",
-    options: ["Steamboat Willie", "Mickah Mouse", "Captain Steve", "Mickey"],
-    answers: 0,
-    
-  },
-  {
-    question: "What is Micky Mouse's girlfriend, Minnie Mouse's full name?",
-    options: ["Minnie Mouse", "Minerva Mouse", "Minilla Mouse", "Eminem"],
-    answers: 1,
-    
-  },
-  {
-    question: "What is Mickey Mouse's dog name?",
-    options: ["Jupiter", "Comet", "Pluto", "Venus"],
-    answers: 2,
-  },
+var quiz = [{
+        question: "In what year was Mickey Mouse first introduced to the public.",
+        options: ["1958", "1982", "1930", "1928"],
+        correctAnswer: 3,
+        userAnswer: null
+    },
+    {
+        question: "What was Micky Mouse's original name?",
+        options: ["Steamboat Willie", "Mickah Mouse", "Captain Steve", "Mickey"],
+        correctAnswer: 0,
+        userAnswer: null
+
+    },
+    {
+        question: "What is Micky Mouse's girlfriend, Minnie Mouse's full name?",
+        options: ["Minnie Mouse", "Minerva Mouse", "Minilla Mouse", "Eminem"],
+        correctAnswer: 1,
+        userAnswer: null
+
+    },
+    {
+        question: "What is Mickey Mouse's dog name?",
+        options: ["Jupiter", "Comet", "Pluto", "Venus"],
+        correctAnswer: 2,
+        userAnswer: null
+    },
 ];
 
-//add event listener "click" to link start button to questions
+// //add event listener "click" to link start button to questions
 
-startButton.addEventListener("click", function(setTime) {
-var secondsLeft = 60; 
-
-//when start button is clicked, timer starts counting backward from .60
-function setTime() {
-  var timerInterval = setInterval(function () {
-    secondsLeft--;
-    timer.textContent = "Timer: " + secondsLeft;
-
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
-
-      
-    }
-  }, 1000);
-  
-}
-setTime();
-startQuiz();
-})
+//Event listener. When click, it's going to start the quiz function
+startButton.addEventListener("click", startQuiz);
 
 var index = 0;
 
-function startQuiz(){
-    
-    var currentQuestion = quiz[index];
-    openingPage.textContent = currentQuestion.question;
-  //loop to pull questions and answers from the array.
-  for (var i = 0; i , currentQuestion.options.length; i++){
-    var choice = currentQuestion.options[i];
-    var newButton = document.createElement("button");
-    newButton.textContent = options;
-    instructions.appendChild(newButton);
+//calls timer function, hides start button, and calls the question
+function startQuiz() {
+  startTimer();
+  nextQuestion();
+
+  buttonDiv.style.display = "none";
+}
 
 
+var currentQuizIndex = 0;
+function nextQuestion(){
+  //takes index of quiz questions
+    var currentQuestion = quiz[currentQuizIndex];
+    // if there is no current question, then the quiz is over
+    if(!currentQuestion){
+      //calls end quiz function
+      endQuiz();
+      //return will end the next question function and return that value.
+      return null;
     }
-  }
+
+  //answers content is cleared from the page  
+  answers.textContent = "";
+  //instructions text takes place of new question
+  instructions.textContent = currentQuestion.question;
+
+  //add for loop to pull questions and answers from the quiz array
+}
+
+
+function showQuizResults(){
+//insert for loop to compare results
+
+//if then statement 
+
+}
+
+var secondsLeft = 30;
+var timerInterval;
+function startTimer(){
+  timerInterval = setInterval(function(){
+    secondsLeft--;
+    timer.textContent = "Timer: " + secondsLeft;
+
+    if (secondsLeft === 0){
+      endTimer();
+    }
+  }, 1000);
+}
 
 
 
-  
-  
+function endTimer(){
+//clears timer
+
+}
+
+
+//calls end timer and shows results
+function endQuiz(){
+  endTimer();
+  showQuizResults();
+}
